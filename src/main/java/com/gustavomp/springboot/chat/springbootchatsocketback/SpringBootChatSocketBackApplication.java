ae62924226390e95d141e7bb1d8217596d2badfc
gustavo9601 <tavo9601@gmail.com>
2022-08-21 00:16:22 -0500

    init projet

---
+++ src/main/java/com/gustavomp/springboot/chat/springbootchatsocketback/SpringBootChatSocketBackApplication.java
@@ -0,0 +1,13 @@
+package com.gustavomp.springboot.chat.springbootchatsocketback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatSocketBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChatSocketBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavomp/springboot/chat/springbootchatsocketback/SpringBootChatSocketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavomp.springboot.chat.springbootchatsocketback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatSocketBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
